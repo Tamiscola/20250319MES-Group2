5af5202f64292714ff958e2b332a0210aa4a9b02
maengsky <mhaneul46@gmail.com>
2025-04-01 12:10:53 +0900

    제품 관리 파일 업로드
    controller, service, product-list.html, search - ProductSearch/ProductSearchImpl, pageDTO - pageRequestDTO/pageResponseDTO 업로드
    제품 관리 페이지 - 검색, 수정, 삭제, 등록 기능 구현

---
+++ 20250319MES-Group2/src/main/java/org/example/projects/controller/ProductService.java
@@ -0,0 +1,95 @@
+package org.example.projects.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.example.projects.domain.Product;
+import org.example.projects.domain.ProductionLine;
+import org.example.projects.domain.ProductionPlan;
+import org.example.projects.domain.enums.Status;
+import org.example.projects.dto.ProductDTO;
+import org.example.projects.dto.pageDTO.PageRequestDTO;
+import org.example.projects.dto.pageDTO.PageResponseDTO;
+import org.example.projects.repository.ProductRepository;
+import org.example.projects.repository.ProductionLineRepository;
+import org.example.projects.repository.ProductionPlanRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+    private final ModelMapper modelMapper;
+
+    @Transactional
+    public String productRegister(ProductDTO productDTO) {
+        Product product = Product.builder()
+                .productName(productDTO.getProductName())
+                .regBy(productDTO.getRegBy())
+                .regDate(LocalDate.now())
+                .quantity(0)
+                .build();
+
+        String productName = productRepository.save(product).getProductName();
+
+        return productName;
+    }
+
+    @Transactional
+    public void productModify(ProductDTO productDTO) {
+        Optional<Product> result = productRepository.findByProductId(productDTO.getProductId());
+
+        Product product = result.orElseThrow();
+        log.info("Product Retrieved: " + product);
+
+        product.change(productDTO.getProductName(),
+                productDTO.getRegBy(),
+                productDTO.getRegDate()
+        );
+
+        productRepository.save(product);
+    }
+
+    @Transactional
+    public void productRemove(String productId) {
+        productRepository.deleteById(productId);
+    }
+
+    @Transactional
+    public PageResponseDTO<ProductDTO> list(PageRequestDTO pageRequestDTO) {
+        String[] types = pageRequestDTO.getTypes();
+        String keyword = pageRequestDTO.getKeyword();
+        Pageable pageable = pageRequestDTO.getPageable("productCode");
+
+        Page<Product> result = productRepository.searchAll(types, keyword, pageable);
+
+        List<ProductDTO> dtoList = result.getContent().stream()
+                .map(product -> {
+                    ProductDTO dto = modelMapper.map(product, ProductDTO.class);
+                    log.info("매핑된 ProductDTO: {}", dto);
+                    return dto;
+                })
+                .collect(Collectors.toList());
+
+        return PageResponseDTO.<ProductDTO>withAll()
+                .pageRequestDTO(pageRequestDTO)
+                .dtoList(dtoList)
+                .total((int)result.getTotalElements())
+                .build();
+    }
+
+    public Product getProductById(String id) {
+        Product product = productRepository.findById(id).orElse(null);
+
+        return product;
+    }
+}
